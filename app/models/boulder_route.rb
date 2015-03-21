666254b8d53b39a21d3ef514879aef637f0ce0ec
Spyder McHenry <spyder@notch8.com>
2015-03-21 14:42:50 -0700

    sets up pages controller, updates asset pipe

--- app/models/boulder_route.rb
+++ app/models/boulder_route.rb
@@ -3,5 +3,4 @@ class BoulderRoute < ActiveRecord::Base
   belongs_to :wall
   has_attached_file :route_picture, :styles => { :medium => "400x400>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :route_picture, :content_type => /\Aimage\/.*\Z/
-
 end
